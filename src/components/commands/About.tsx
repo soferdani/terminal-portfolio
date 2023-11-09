cbc515a32eb89db87540025f11ab160e7fe67f2b
Dani <dani@innovi.co.il>
2023-11-09 13:19:50 +0200

    fix some commends

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -8,11 +8,10 @@ const About: React.FC = () => {
   return (
     <AboutWrapper data-testid="about">
       <p>
-        Hi, my name is <HighlightSpan>Sat Naing</HighlightSpan>!
+        Hi, my name is <HighlightSpan>Dani sofer</HighlightSpan>!
       </p>
       <p>
-        I'm <HighlightAlt>a full-stack developer</HighlightAlt> based in Yangon,
-        Myanmar.
+        I'm <HighlightAlt>a full-stack developer</HighlightAlt> currently based in Herzeliya, Israel
       </p>
       <p>
         I am passionate about writing codes and <br />
--- src/components/commands/Education.tsx
+++ src/components/commands/Education.tsx
@@ -17,17 +17,17 @@ const Education: React.FC = () => {
 
 const eduBg = [
   {
-    title: "B.Sc (Hons) in Computing",
-    desc: "Edinburgh Napier University | 2018 ~ 2019",
+    title: "B.Sc in Industrial Engineering & Management",
+    desc: "Shenkar - Engineering. Design. Art. | 2015 ~ 2019",
   },
-  {
-    title: "HND in Computing & System Development",
-    desc: "Info Myanmar University | 2016 - 2018",
-  },
-  {
-    title: "IELTS 6.5",
-    desc: "British Council Myanmar | 2017",
-  },
+  // {
+  //   title: "HND in Computing & System Development",
+  //   desc: "Info Myanmar University | 2016 - 2018",
+  // },
+  // {
+  //   title: "IELTS 6.5",
+  //   desc: "British Council Myanmar | 2017",
+  // },
 ];
 
 export default Education;
